a7ccecad9f4fb0a4db555e8d8e7247baeb82a882
saitejavunnava3377 <146068496+saitejavunnava3377@users.noreply.github.com>
2023-09-26 13:53:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var score=0;
+
+function decrease(){
+    score=score-5;
+    document.getElementById("scorelabel").textContent=score;
+}
+
+function increase(){
+    score=score+5;
+    document.getElementById("scorelabel").textContent=score;
+}
